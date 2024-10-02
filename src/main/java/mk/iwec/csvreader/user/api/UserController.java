de87074e18aec71b854bbc77a4567de7bdc5bd3c
desanoski-filip <desanoskifilip@gmail.com>
2024-10-02 17:26:10 +0200

    .

--- src/main/java/mk/iwec/csvreader/user/api/UserController.java
+++ src/main/java/mk/iwec/csvreader/user/api/UserController.java
@@ -6,6 +6,7 @@ import mk.iwec.csvreader.user.model.User;
 import mk.iwec.csvreader.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(EndPoints.USER)
+@Validated
 public class UserController {
 
     private final UserService userService;
